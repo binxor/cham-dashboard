8808d7c58b8e7bc4aba29b891f505334cc33fa84
Kimberly Stanley <binkxor@gmail.com>
2017-09-20 20:02:11 -0700

    Changing permissions.

--- cham/src/components/Chart.js
+++ cham/src/components/Chart.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 class Chart extends Component {
   render() {
     return (
-      <div style={{border:'solid green 1px'}}><h3>CHART GOES HERE</h3></div>
+      <div style={{border:'solid green 1px'}}>
+        <h3>CHART GOES HERE</h3>
+      </div>
     )
   }        
 }
