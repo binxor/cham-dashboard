43e88aa439ab4220e556e4022501f406c0da61df
Unknown <binkxor@gmail.com>
2017-10-01 13:22:08 -0700

    Adding hapi server, sqlite3 query, and config file.

---
+++ cham/server/server.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const Hapi = require('hapi');
+var sqlite3 = require('sqlite3').verbose(); 
+var constants = require('../config/config.js'); 
+var dbloc = constants.db.location; 
+var db = new sqlite3.Database(dbloc); 
+
+const server = new Hapi.Server();
+server.connection({ port: 3001, host: 'localhost' });
+
+server.route([{
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('OK');
+    }
+},{
+    method: 'GET',
+    path: '/data/params',
+    handler: function (request, reply) {
+        reply (new Promise((rsv, rej) => {
+
+            db.serialize(function() { 
+            
+                db.all("SELECT * FROM params", function(err, data) { 
+                    if(err) rej(err)
+                    else rsv(data);
+                }); 
+
+            }); 
+        })) 
+    } 
+}]);
+
+
+server.start((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log(`Server running at: ${server.info.uri}`);
+});
