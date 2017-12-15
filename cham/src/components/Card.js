df7c12d65fe10f59dd54431a83bbb95376775378
Unknown <binkxor@gmail.com>
2017-12-14 18:51:46 -0800

    Adding slider component and dropdown detail
    Cards now have a clickable "Detail" view to show allowable range of parameter, current value.

--- cham/src/components/Card.js
+++ cham/src/components/Card.js
@@ -3,19 +3,52 @@ import 'Card.css';
 import FetchData from 'FetchData.js';
 
 class Card extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        detailsVisible: false
+    };
+
+    this.showDetails = this.showDetails.bind(this);
+  }
+
+  showDetails = function(e) {
+    e.stopPropagation();
+    e.nativeEvent.stopImmediatePropagation();
+    this.setState((prevState) => ({ 
+      detailsVisible: !prevState.detailsVisible 
+    }));
+
+  }
+
   render() {
     const unitSize = {
       fontSize: 0.8 +'em'
     };
+    const extend = {
+      height: 100 + 'px',
+      padding: 10 + 'px',
+      background: 'black',
+      position: 'absolute',
+      width: 100 + '%',
+      zIndex: 1000
+    };
     return (
-      <div className={"Card " + this.props.className}>
+      <div className={"Card " + this.props.className} onClick={this.showDetails}>
         <div><h3>{this.props.data.title}</h3></div>
         <div >
           <h1 className='em0p5'>{this.props.data.val}
             <span style={unitSize}>{this.props.data.unit}</span>
           </h1>
-          <FetchData name={this.props.data.title} />
         </div>
+        {!this.state.detailsVisible && 'Detail'}
+          {this.state.detailsVisible && (
+            <div style={extend}>
+            <FetchData name={this.props.data.title} currentVal={this.props.data.val} />
+          </div>
+         )}
+        
       </div>
     )
   }        
--- cham/src/components/FetchData.js
+++ cham/src/components/FetchData.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import axios from 'axios';
+import Slider from 'react-rangeslider';
+import 'react-rangeslider/lib/index.css';
 
 class FetchData extends React.Component {
     constructor(props) {
@@ -8,6 +10,13 @@ class FetchData extends React.Component {
         this.state = {
             data: []
         };
+
+        this.buildLabel = function(min, max) {
+            let labelObj = {};
+            labelObj[min] = min;
+            labelObj[max] = max;
+            return labelObj;
+        }
     }
 
     componentDidMount() {
@@ -19,11 +28,31 @@ class FetchData extends React.Component {
     }
 
     render() {
+        function showMessage(min, max, val) {
+            if(val < min) {
+                return <p style={{color: 'magenta'}}> Out of Range! </p>
+            } else if(val > max) {
+                return <p style={{color: 'orange'}}> Out of Range! </p>
+            } else {
+                return <p style={{color: 'yellowGreen'}}> In Range </p>
+            }
+
+        }
         return (
             <div>
                 {this.state.data.map(d => { 
                     if(d.name.toLowerCase() === this.props.name.toLowerCase()) {
-                      return  <span key={d.id} id={d.name}>{d.min} - {d.max}</span>
+                      return    <span key={d.id} id={d.name}>
+                                    {showMessage(d.min, d.max, this.props.currentVal)}
+                                    <Slider value={this.props.currentVal} 
+                                        orientation="horizontal" 
+                                        min={d.min}
+                                        max={d.max}
+                                        labels={this.buildLabel(d.min,d.max)}           
+                                        handleLabel={this.props.currentVal.toString()}
+                                        tooltip={true}
+                                    />
+                                </span>
                     } else {
                         return '';
                     }
