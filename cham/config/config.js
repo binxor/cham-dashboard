3bf7229c467633a867765b3be5a6671a76cafdc8
binkxor <binkxor@gmail.com>
2020-05-27 11:14:01 -0700

    Env config

--- cham/config/config.js
+++ cham/config/config.js
@@ -1,6 +1,6 @@
 var constants = {
     devOrProd: 'prod',
-    dev: {
+    local: {
         params: {
             location: '../data/ex1'
         },
@@ -12,7 +12,7 @@ var constants = {
             port: 3001
         }
     },
-    prod: {
+    dev: {
         params: {
             location: '../data/ex1'
         },
@@ -21,6 +21,18 @@ var constants = {
         },
         server: {
             ip: '10.0.0.22',
+            port: 3001
+        }
+    },
+    prod: {
+        params: {
+            location: '../data/ex1'
+        },
+        readings: {
+            location: '../data/sensordata.db'
+        },
+        server: {
+            ip: '98.246.239.190',
             port: 3001
         }
     }
